86b1961fe60a8bd3f19f7de09bb115bab9e50797
OrShemes1992 <chikos2011@gmail.com>
2021-06-15 11:01:17 +0300

    calculte primium

--- calculter.php
+++ calculter.php
@@ -3,14 +3,35 @@
 <body>
 
 
-<form action="calculter.php" method="get">
-    <input type="number" name="num1">
+<form action="calculter.php" method="post">
+    First Num: <input type="number" step="0.1" name="num1">
     <br>
-    <input type="number" name="num2">
+    OP: <INPUT TYPE="textbox" name="op">
+    <br>
+    Second Num: <input type="number"  step="0.1"  name="num2">
+    <br>
     <input type="submit">
 </form>
 
-Answer: <?PHP echo $_GET["num1"] + $_GET["num2"] ?>
+Answer: <?PHP
+echo "<br>";
+$num1 = $_POST["num1"];
+$num2 = $_POST["num2"];
+$op = $_POST["op"];
+
+if($op == "+"){
+echo $num1 + $num2;
+}elseif ($op == "-"){
+    echo $num1 - $num2;
+}elseif ($op == "*"){
+    echo $num1 * $num2;
+}elseif ($op == "/"){
+    echo $num1 / $num2;
+}else{
+    echo "Invalid Operator";
+}
+
+?>
 
 </html>
 </body>
